a454714da9b250f5949a55d93199e9775d8811a4
Koulinn <drdverzola@gmail.com>
2021-06-25 22:04:14 +0100

    Home Page Dynamically generated

--- js/index.js
+++ js/index.js
@@ -21,6 +21,41 @@ function createHtmlContent(HTMLTag, innerText, ArrClassesToAdd, HtmlNodeSelector
 }
 
 
+let dataFromAPI
+
+
+const getDataFromAPI = async (query) => {
+    try {
+        let response = await fetch("https://striveschool-api.herokuapp.com/api/deezer/search?q=" + query)
+        let dataRequested = await response.json()
+        return dataRequested
+    } catch (e) {
+        return e
+    }
+}
+
+const sendRequestToApi = async (userSearchValue) => {
+    dataFromAPI = await getDataFromAPI(userSearchValue)
+    loadPageHTML()
+}
+
+
+function loadPageHTML() {
+    generateSections('Best_Music', 0, 7)
+    generateSections('Chilling', 7, 14)
+    generateSections('Relax', 14, 21)
+
+}
+
+window.onload = () => {
+    sendRequestToApi('sun')
+}
+
+
+
+
+
+
 
 
 
@@ -29,57 +64,92 @@ function createHtmlContent(HTMLTag, innerText, ArrClassesToAdd, HtmlNodeSelector
  * @param {String} sectionIdName , should has no space
  */
 
-let test
-function generateSections(sectionIdName) {
+function generateSections(sectionIdName, sliceInitial, sliceFinal) {
     const sectionClasses = ['row', 'd-flex', 'flex-column', 'mt-3', 'py-0', 'px-4', 'm-0']
     let newSection = createHtmlContent('section', '', sectionClasses,
         'main', 'beforeend')
     newSection.id = sectionIdName
     generateSectionTitle(sectionIdName)
+    generateCards(sectionIdName, sliceInitial, sliceFinal)
 }
 
-generateSections('Best_Music')
 
-function generateSectionTitle(sectionTitle){
+function generateSectionTitle(sectionTitle) {
     let sectionContainer = document.querySelector(`#${sectionTitle}`)
     let sectionTitleRemoved_ = sectionTitle.replace('_', ' ')
-  
+
 
 
     sectionContainer.insertAdjacentHTML('afterbegin', `
          <div class="col-12 p-0 d-flex justify-content-between align-items-center">
             <div class="d-flex flex-column">
             <h2 class="m-0">
-              <a>${sectionTitleRemoved_}</a>
+              ${sectionTitleRemoved_}
             </h2>
             <span class="section-text-aux mt-1 mb-2">Latest music</span>
              </div>
 
           <span class="seeMore">SEE ALL</span>
         </div>
-        <div class="col-12 d-flex px-0 card-decker"></div>
+        <div class="col-12 d-flex px-0 cardDeck"></div>
         `)
 }
 
-function generateCards(sectionToInsert){
-    let sectionContainer = document.querySelector(`#${sectionToInsert} .card-decker`)
-    sectionContainer.insertAdjacentHTML('afterbegin',`
-            <div class="card col d-flex flex-nowrap card-square">
-                <img src="https://images.squarespace-cdn.com/content/v1/57a5229820099e8bfa2664b8/1582922019177-R5SJ6SKIQANSSBC989W2/ke17ZwdGBToddI8pDm48kJe0qEoc2cIEoNCZEG6czGwUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcNi17y6wQb1iplGo-NkoX1FIVeq1dKD15mhhBTseqgkUqQgLJ-z95FSy_0lgEAXPe/bossa-nova.JPG?format=500w" class="card-img-top" alt="...">
+function generateCards(sectionToInsert, sliceInitial, sliceEnd) {
+    let sectionContainer = document.querySelector(`#${sectionToInsert} .cardDeck`)
+    let data = dataFromAPI.data.slice(sliceInitial, sliceEnd)
+    test = data
+
+    data.forEach((data, index) => {
+        sectionContainer.insertAdjacentHTML('afterbegin', `
+            <div class="d-flex card col flex-nowrap card-square">
+               <a href="albumPage.html?album_Id=${data.album.id}">
+                    <img src="${data.album.cover_medium}" class="card-img-top" alt="...">
+                </a>
                 <div class="card-body card-bodymod px-0">
-                <h5 class="card-title card-title-mod">Card title</h5>
-                <p class="card-text card-text-mod">Some quick example text to build on the card title and make up the bulk
-                    of the card's content.</p>
+                <a href="albumPage.html?album_Id=${data.album.id}">
+                    <h5 class="card-title card-title-mod">${data.album.title}</h5>
+                </a>
+                <a href="artistPage.html?album_Id=${data.album.id}">
+                     <p class="card-text card-text-mod">${data.artist.name}</p>
+                </a>
                 </div>
             </div>
-    
-    `)
+        `)
+    })
+    addColClassesToCards(sectionToInsert)
 
 }
 
 
+function addColClassesToCards(sectionTitle) {
+    let allCards = document.querySelectorAll(`#${sectionTitle} .cardDeck > div`)
+    allCards.forEach((card, index) => {
+
+        if (index === 2) {
+            card.classList.remove(`d-flex`)
+            card.classList.add(`d-none`)
+            card.classList.add(`d-md-flex`)
+        }
+
+        if (index >= 3 & index <= 4) {
+            card.classList.remove(`d-flex`)
+            card.classList.add(`d-none`)
+            card.classList.add(`d-lg-flex`)
+        }
+
+        if (index >= 4) {
+            card.classList.remove(`d-flex`)
+            card.classList.add(`d-none`)
+            card.classList.add(`d-xxl-flex`)
+        }
+    })
+}
 
 
+//card col flex-nowrap card-square d-none d-md-flex
+
+//card col flex-nowrap d-none d-xxl-flex card-square
 
 
 
@@ -98,12 +168,11 @@ function generateCards(sectionToInsert){
 const btnCreatePlaylist = document.querySelector(
     "#nav-sideBar > div.sub-menu.mt-4 > ul > li:nth-child(1) > button"
 );
-btnCreatePlaylist.addEventListener("click", function() {
-    console.log("clickdsdss"),
-        createHtmlContent(
-            "li",
-            "Text Dynamically gen", [`w-100`, `pl-4`],
-            "#nav-sideBar #scrolling-menu .scroll > ul",
-            "beforeend"
-        );
+btnCreatePlaylist.addEventListener("click", function () {
+    createHtmlContent(
+        "li",
+        "Text Dynamically gen", [`w-100`, `pl-4`],
+        "#nav-sideBar #scrolling-menu .scroll > ul",
+        "beforeend"
+    );
 });
